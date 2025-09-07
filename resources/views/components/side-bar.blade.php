4ee6612c90840b0ea134b827724c606ebc670e4b
JHenrique <henryque.pc.game@gmail.com>
2025-09-07 15:21:53 -0300

    sidebar responsivel

--- resources/views/components/side-bar.blade.php
+++ resources/views/components/side-bar.blade.php
@@ -1,8 +1,16 @@
 <aside class="col-md-2 text-bg-dark" id="navbarNavd">
-    <div class="d-flex flex-column mt-5 navbar-collapse collapse-horizontal">
-        <div class="d-flex flex-column align-self-center p-1">
+    <div class="offcanvas-lg offcanvas-start text-bg-dark" tabindex="-1" id="atSidebar" aria-labelledby="offcanvasScrollingLabel">
+        <div class="offcanvas-header">
+{{--            <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>--}}
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" data-bs-target="#atSidebar"></button>
+        </div>
 
-              <a href="{{ route('home') }}" class="nav-link"><i class="fas fa-home me-3"></i>Home</a>
+
+    <div class="offcanvas-body">
+
+        <div class="d-flex flex-column mt-5 navbar-collapse collapse-horizontal">
+            <div class="d-flex flex-column align-self-center p-1">
+                <a href="{{ route('home') }}" class="nav-link"><i class="fas fa-home me-3"></i>Home</a>
                 @can('admin')
                     <a href="{{ route('colaboration') }}" class="nav-link mt-4"><i class="fa-solid fa-people-group me-3"></i>Colaboradores</a>
                 @endcanany
@@ -15,18 +23,21 @@
                 @endcan
                 <a href="{{ route('user.profile') }}" class="nav-link mt-4"><i class="fa-solid fa-user-gear me-3"></i>Editar perfil</a>
 
-        </div>
-        <hr>
-        <div class="text-center">
+            </div>
+            <hr>
+            <div class="text-center">
 
-            <form action="{{ route('logout') }}" method="post">
-                @csrf
-                <button type="submit" class="btn btn-sm btn-outline-danger">
-                    <i class="fas fa-sign-out-alt me-3"></i> Sair
-                </button>
-            </form>
+                <form action="{{ route('logout') }}" method="post">
+                    @csrf
+                    <button type="submit" class="btn btn-sm btn-outline-danger">
+                        <i class="fas fa-sign-out-alt me-3"></i> Sair
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
+</div>
 </aside>
+
 
 
--- resources/views/components/user-bar.blade.php
+++ resources/views/components/user-bar.blade.php
@@ -1,16 +1,18 @@
 <nav class="navbar navbar-expand-lg text-bg-primary pe-4 ps-4">
         <div class="container-fluid ">
-            <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarNavd"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                <span class="navbar-toggler-icon"></span>
-            </button>
+{{--            <button--}}
+{{--                class="navbar-toggler"--}}
+{{--                type="button"--}}
+{{--                data-bs-toggle="collapse"--}}
+{{--                data-bs-target="#navbarNavd"--}}
+{{--                aria-controls="navbarNav"--}}
+{{--                aria-expanded="false"--}}
+{{--                aria-label="Toggle navigation"--}}
+{{--            >--}}
+{{--                <span class="navbar-toggler-icon"></span>--}}
+{{--            </button>--}}
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#atSidebar" aria-controls="bd Sidebar" aria-label="Toggle docs navegation"><span class="navbar-toggler-icon"></span></button>
+
             <a class="navbar-brand" href="{{ route('home') }}">Agendamento-Onlyne</a>
             <div class="d-flex justify-items-end align-items-center pe-4" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
